04a2f9cd93d0e68eb28a6b5dbc451bdaef17d94f
samdiron <samer1787rizk@gmail.com>
2025-04-14 19:44:56 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,108 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+
 import "./index.css";
-
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+export default function App() {
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
-  );
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
 
-export default App;
+
+
+//
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import { invoke } from "@tauri-apps/api/core";
+// import "./index.css";
+//
+// function App() {
+//   const [greetMsg, setGreetMsg] = useState("");
+//   const [name, setName] = useState("");
+//
+//   async function greet() {
+//     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//     setGreetMsg(await invoke("greet", { name }));
+//   }
+//
+//   return (
+//     <main className="container">
+//       <h1>Welcome to Tauri + React</h1>
+//
+//       <div className="row">
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+//         </a>
+//         <a href="https://tauri.app" target="_blank">
+//           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+//         </a>
+//         <a href="https://reactjs.org" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+//
+//       <form
+//         className="row"
+//         onSubmit={(e) => {
+//           e.preventDefault();
+//           greet();
+//         }}
+//       >
+//         <input
+//           id="greet-input"
+//           onChange={(e) => setName(e.currentTarget.value)}
+//           placeholder="Enter a name..."
+//         />
+//         <button type="submit">Greet</button>
+//       </form>
+//       <p>{greetMsg}</p>
+//     </main>
+//   );
+// }
+//
+// export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import AppSidebar from "./components/app-sidebar";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <main class="bg-black h-screen ">
+    <main class="bg-black h-screen text-white">
       <App />
 
     </main>
