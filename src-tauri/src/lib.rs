681a6ba75c891209c258198cacdc4d843555dde2
samdiron <samer1787rizk@gmail.com>
2025-05-06 23:40:00 +0300

    doing shit

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,8 +4,12 @@ use std::sync::Mutex;
 use common_lib::tokio;
 use lib_db::sqlite::get_sqlite_conn;
 use lib_db::types::SqlitePool;
+use lib_db::inner::{query, Row};
 use serde::Serialize;
 use serde::Deserialize;
+
+
+
 
 
 
@@ -37,9 +41,42 @@ async fn set_db_path(path: String) -> bool {
 }
 
 
+use lib_db::escape_user_input; 
 
+#[tauri::command]
+async fn list_media(u: User) -> Vec<Media> {
+    let pool = DB_POOL.lock().unwrap().clone();
+    let cpid = escape_user_input(&u.cpid);
+    let sql = format!(" SELECT * from media WHERE cpid = '{cpid}' ;" );
+    let res = query(&sql).fetch_all(&pool).await.unwrap();
+    let mut media_vec: Vec<Media> = vec![];
+    for r in res {
+        let name: String = r.get("name");
+        let cpid: String = r.get("cpid");
+        let path: String = r.get("path");
+        let type_: String = r.get("type");
+        let in_host: String = r.get("host");
+        let checksum: String = r.get("checksum");
 
-// async fn list_users 
+        let date: i64 = r.get("date");
+        let size: i64 = r.get("size");
+
+        let media = Media {
+            name,
+            cpid,
+            size,
+            date,
+            path,
+            type_,
+            in_host,
+            checksum,
+        };
+        media_vec.push(media);
+
+    }
+    media_vec
+}
+
 
 
 
@@ -64,27 +101,22 @@ struct Server {
 #[derive(Serialize, Deserialize)]
 struct Media {
     name: String,
-    ext: String,
-    checksum: String,
+    cpid: String,
+    size: i64,
+    date: i64,
+    path: String,
+    type_: String,
     in_host: String,
+    checksum: String,
 
 }
-
-
-
-
-
-
-
-
-
 
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![set_db_path])
+        .invoke_handler(tauri::generate_handler![set_db_path, list_media])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
