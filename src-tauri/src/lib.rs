57566e11d4d1607822aa88a43a6d69bafb52e100
samdiron <samer1787rizk@gmail.com>
2025-05-07 14:04:23 +0300

    update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,18 @@
+use std::io::Result;
 use std::sync::LazyLock;
 use std::sync::Mutex;
 
 use common_lib::tokio;
 use lib_db::sqlite::get_sqlite_conn;
+use lib_db::sqlite::sqlite_host::SqliteHost;
+use lib_db::sqlite::sqlite_media::SqliteMedia;
+use lib_db::sqlite::sqlite_user::SqliteUser;
 use lib_db::types::SqlitePool;
 use lib_db::inner::{query, Row};
 use serde::Serialize;
 use serde::Deserialize;
 
+use lib_db::escape_user_input; 
 
 
 
@@ -41,34 +46,97 @@ async fn set_db_path(path: String) -> bool {
 }
 
 
-use lib_db::escape_user_input; 
+#[tauri::command]
+async fn login(
+    username: String,
+    server_name: String,
+    host_name: String,
+) -> Result<(SqliteUser, SqliteHost)> {
+    let pool = DB_POOL.lock().unwrap().clone();
+    let is_host = get_server(&server_name, &host_name, &pool).await;
+    if is_host.is_ok() {
+        let host = is_host.unwrap();
+        let username = escape_user_input(&username);
+        let sql = format!(" SELECT * FROM user WHERE username = '{username}' AND host = '{}' ", &host.cpid);
+        let res = query(&sql).fetch_one(&pool).await;
+        if res.is_ok() {
+            let name: String = res.get("name");
+            let cpid: String = res.get("cpid");
+            let host: String = res.get("host");
+            let email: String = res.get("email");
+            let usrname: String = res.get("usrname");
+            let user = SqliteUser {
+                name,
+                host,
+                cpid,
+                email,
+                usrname
+            };
+            Ok((user, host))
+
+        }else {};
+    }else {}
+
+    
+}
+
+
+async fn get_server(
+    server_name: &String,
+    host_name: &String,
+    pool: &SqlitePool
+) -> lib_db::inner::Result<SqliteHost> {
+    let name = escape_user_input(server_name);
+    let host = escape_user_input(host_name);
+    let sql = format!(" SELECT * FROM host WHERE name = '{name}' AND host = '{host}' ;");
+    let res = query(&sql).fetch_one(pool).await?;
+    
+    let name: String = res.get("name");
+    let host: String = res.get("host");
+    let cpid: String = res.get("cpid");
+    let pub_ip: String = res.get("pub_ip");
+    let pri_ip: String = res.get("pri_ip");
+    let port: u16 = res.get("port");
+
+    let host = SqliteHost {
+        name,
+        cpid,
+        host,
+        port,
+        pub_ip,
+        pri_ip,
+    };
+    Ok(host)
+        
+}
+
 
 #[tauri::command]
-async fn list_media(u: User) -> Vec<Media> {
+async fn list_media(user_cpid: String) -> Vec<SqliteMedia> {
     let pool = DB_POOL.lock().unwrap().clone();
-    let cpid = escape_user_input(&u.cpid);
-    let sql = format!(" SELECT * from media WHERE cpid = '{cpid}' ;" );
+    let cpid = escape_user_input(&user_cpid);
+    let sql = format!(" SELECT * FROM media WHERE cpid = '{cpid}' ;" );
     let res = query(&sql).fetch_all(&pool).await.unwrap();
-    let mut media_vec: Vec<Media> = vec![];
+    let mut media_vec: Vec<SqliteMedia> = vec![];
     for r in res {
         let name: String = r.get("name");
         let cpid: String = r.get("cpid");
         let path: String = r.get("path");
         let type_: String = r.get("type");
-        let in_host: String = r.get("host");
+        let host: String = r.get("host");
         let checksum: String = r.get("checksum");
 
         let date: i64 = r.get("date");
         let size: i64 = r.get("size");
 
-        let media = Media {
+        let media = SqliteMedia {
             name,
             cpid,
             size,
             date,
             path,
             type_,
-            in_host,
+            host,
             checksum,
         };
         media_vec.push(media);
@@ -89,34 +157,17 @@ struct User {
     cpid: String
 }
 
-#[derive(Serialize, Deserialize)]
-struct Server {
-    name: String,
-    host: String,
-    cpid: String,
-    pub_ip: String,
-    pri_ip: String,
-}
 
-#[derive(Serialize, Deserialize)]
-struct Media {
-    name: String,
-    cpid: String,
-    size: i64,
-    date: i64,
-    path: String,
-    type_: String,
-    in_host: String,
-    checksum: String,
-
-}
 
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![set_db_path, list_media])
+        .invoke_handler(tauri::generate_handler![
+            set_db_path,
+            list_media,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
